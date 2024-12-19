fb9ab6c2e2823dddf736463b7426ec7d36cc8672
Susovan Mishra <143598676+Sovanmista@users.noreply.github.com>
2024-12-19 17:21:26 +0530

    Update index.js

--- backendcodes/index.js
+++ backendcodes/index.js
@@ -5,7 +5,11 @@ const Router = require("./routes/rout");
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb+srv://susovanmishra0800:<x2hZjeo9dIacA9xu>@cluster0.49vu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://susovanmishra0800:<x2hZjeo9dIacA9xu>@cluster0.49vu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));;
 app.use("/api", Router);
 app.listen(3001, () => {
   console.log("listening on 3001");
