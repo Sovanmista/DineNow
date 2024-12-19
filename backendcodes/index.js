f00ddebdd7714dbd046196f00c2ad312150a3820
Susovan Mishra <143598676+Sovanmista@users.noreply.github.com>
2024-12-19 19:21:35 +0530

    Update index.js

--- backendcodes/index.js
+++ backendcodes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const Router = require("./routes/rout"); // Ensure this path is correct
+const Router = require("./routes/rout");
 const app = express();
 
 app.use(cors());
